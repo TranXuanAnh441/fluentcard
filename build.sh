b088ccb363e0edf459477863f29c6edbf50eb5a1
TranXuanAnh441 <tx.anh2002@gmail.com>
2023-10-15 12:41:25 +0900

    update db setting for deployment

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Create a virtual environment
+echo "Creating a virtual environment..."
+python3.9 -m venv venv
+source venv/bin/activate
+
 # Install the latest version of pip
 echo "Installing the latest version of pip..."
 python -m pip install --upgrade pip
@@ -7,6 +12,11 @@ python -m pip install --upgrade pip
 # Build the project
 echo "Building the project..."
 python -m pip install -r requirements.txt
+
+# Make migrations
+echo "Making migrations..."
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 # Collect static files
 echo "Collecting static files..."
