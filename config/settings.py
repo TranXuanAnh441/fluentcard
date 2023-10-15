3d8fdbb9159feeeda2ce494d673cc0d42c071a00
TranXuanAnh441 <tx.anh2002@gmail.com>
2023-10-15 16:02:59 +0900

    add port setting

--- config/settings.py
+++ config/settings.py
@@ -86,6 +86,7 @@ DATABASES = {
         'NAME': os.environ.get("NAME"),
         'USER': os.environ.get("USER"),
         'PASSWORD': os.environ.get("PASSWORD"),
+        'PORT': os.environ.get("PORT"),
     }
 }
 
