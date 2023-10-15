4b64220eca9c9164292ad9d770457f292b7574cd
TranXuanAnh441 <tx.anh2002@gmail.com>
2023-10-15 16:27:52 +0900

    add users package

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'config',
     'dictionary',
+    'users',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -136,3 +139,6 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
