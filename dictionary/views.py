d7e216a67ad128b09f8f7fcf221de1f372fca48e
TranXuanAnh441 <tx.anh2002@gmail.com>
2023-10-17 12:20:22 +0900

    decrease worddict db access

--- dictionary/views.py
+++ dictionary/views.py
@@ -17,9 +17,11 @@ def word_search(request):
         words = jisho_word_search(search)
         data['words'] = words
         word_objs = []
-        for word in words:
-            if WordDict.objects.filter(word=word['slug']).count() == 0 and word['hiragana'] and word['kanji'] and word['definitions']:
-                word_objs.append(WordDict(word = word['slug'], definitions = str(word['definitions']), hiragana= word['hiragana'], kanji=word['kanji']))
+
+        if WordDict.objects.filter(word=words[0]['slug']).count() == 0:
+            for word in words:
+                if word['hiragana'] and word['kanji'] and word['definitions']:
+                    word_objs.append(WordDict(word = word['slug'], definitions = str(word['definitions']), hiragana= word['hiragana'], kanji=word['kanji']))
         
         if len(words) > 0:
             WordDict.objects.bulk_create([ obj for obj in word_objs ])
