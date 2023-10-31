9c3ba986404f87eec24489b601d31d0caae5f50b
TranXuanAnh441 <tx.anh2002@gmail.com>
2023-10-31 10:58:23 +0900

    fix button css

--- decks/static/decks/js/deck_progress.js
+++ decks/static/decks/js/deck_progress.js
@@ -5,6 +5,7 @@ for (var i = 0; i < deck_info.length; i++) {
     ratio = parseInt((progress.getAttribute("learnt-card-num"))) / parseInt((progress.getAttribute("card-num"))) * 100;
     if (parseInt((progress.getAttribute("card-num"))) - parseInt((progress.getAttribute("learnt-card-num"))) <= 0) {
         deck_info[i].querySelector("#learn-deck-btn").disabled = true;
+        deck_info[i].querySelector("#learn-deck-btn").style.backgroundColor = "grey";
         deck_info[i].querySelector("#learn-deck-btn a").removeAttribute("href");
     }
     progress.style.setProperty('--bar-width', ratio + '%');
