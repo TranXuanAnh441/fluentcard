2181fbc740f204bde66a36d2c06227793c61b9ba
TranXuanAnh441 <tx.anh2002@gmail.com>
2023-12-03 20:09:18 +0900

    fix divide by 0 error

--- decks/static/decks/js/deck_progress.js
+++ decks/static/decks/js/deck_progress.js
@@ -2,7 +2,10 @@ var deck_info = document.getElementsByClassName("deck-info");
 
 for (var i = 0; i < deck_info.length; i++) {
     var progress = deck_info[i].querySelector(".progress");
-    ratio = parseInt((progress.getAttribute("learnt-card-num"))) / parseInt((progress.getAttribute("card-num"))) * 100;
+    var ratio = 0;
+    if (parseInt((progress.getAttribute("card-num"))) > 0) {
+        ratio = parseInt((progress.getAttribute("learnt-card-num"))) / parseInt((progress.getAttribute("card-num"))) * 100;
+    }
     if (parseInt((progress.getAttribute("card-num"))) - parseInt((progress.getAttribute("learnt-card-num"))) <= 0) {
         deck_info[i].querySelector("#learn-deck-btn").disabled = true;
         deck_info[i].querySelector("#learn-deck-btn").style.backgroundColor = "grey";
