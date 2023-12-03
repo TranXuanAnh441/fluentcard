07c194f2e03ae59e4282d53eb2292c122a0023d5
TranXuanAnh441 <tx.anh2002@gmail.com>
2023-12-03 23:02:28 +0900

    change help page to first page after register

--- users/views.py
+++ users/views.py
@@ -13,7 +13,7 @@ def register_request(request):
 			user = form.save()
 			login(request, user)
 			messages.success(request, "Registration successful." )
-			return redirect('deck_list')
+			return redirect('help')
 		messages.error(request, "Unsuccessful registration. Invalid information.")
 	form = NewUserForm()
 	return render (request=request, template_name="users/register.html", context={"register_form":form})
